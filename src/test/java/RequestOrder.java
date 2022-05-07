b1b256638386fa6d6a2243cea3a74f0409adc6b7
ApecsDm <0133953@bk.ru>
2022-05-07 14:40:34 +0300

    .appveyor.yml modified

--- src/test/java/RequestOrder.java
+++ src/test/java/RequestOrder.java
@@ -20,7 +20,7 @@ public class RequestOrder {
 
     @BeforeAll
     static void setupClass() {
-        WebDriverManager.chromedriver().setup();
+        System.setProperty("webdriver.chrome.driver", "D:\\_Git Projects\\RequestOrder\\driver\\win\\chromedriver.exe");
     }
 
     @BeforeEach
@@ -42,7 +42,7 @@ public class RequestOrder {
         driver.get("http://localhost:9999");
     }
 
-    
+
     /*
     @Test
     void shouldSubmitRequest() {
